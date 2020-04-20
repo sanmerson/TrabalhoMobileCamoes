68a772fac5cad51f56b1e069a40b1c38941d0ef2
Sanmerson Clay <san_merson3@hotmail.com>
2020-04-20 15:57:31 -0300

    Configuração da MainViwer

---
+++ Trabalho Mobile/MobileCamoes/MobileCamoes/Infra/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileCamoes.Infra
+{
+    public class AppSettings
+    {
+        public const string ApiUrl = "https://api.themoviedb.org/3";
+        public const string ApiKey = "be728d12dc07d31f5415023cf4f34297";
+        public const string ApiImageBaseUrl = "https://image.tmdb.org/t/p/w500";
+    }
+}
